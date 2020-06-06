67a788ae277fe59bab0b3e135d44027638f32c42
jacky <jlei0006@student.monash.edu>
2020-06-07 01:56:03 +1000

    update to boundary, validation

--- Boundary.java
+++ Boundary.java
@@ -1,7 +1,7 @@
 public class Boundary {
 	public boolean displayLogin()
 	{
-		System.out.println("********************************************************************************");
+		System.out.println("\f********************************************************************************");
 		System.out.println("\t\t\t\t\t\tWelcome to Mission to Mars");
 		System.out.println("********************************************************************************");
 		System.out.println("1.Log in");
@@ -14,9 +14,30 @@ public class Boundary {
 		System.out.println('\f');
 		System.out.println("System turn off! Have a GOOOOOD Day");
 	}
+
 	public boolean displayLoginFailed() {
 		System.out.println("\fUser Name or Pass Word is wrong");
 		return true;
+	}
+
+	public void displayHomePageForAdmin(String userName){
+		System.out.println("\f********************************************************************************");
+		System.out.println("\t\t\t\t\t\tWelcome to Mission to Mars" + userName);
+		System.out.println("********************************************************************************");
+		System.out.println("1.Create Mission");
+		System.out.println("2.Edit exited Mission");
+		System.out.println("3.Edit exited Criteria");
+		System.out.println("4.Log Out");
+		System.out.println("Please enter your choice:");
+	}
+
+	public void displayHomepageForCoordinator(String userName){
+		System.out.println("\f********************************************************************************");
+		System.out.println("\t\t\t\t\t\tWelcome to Mission to Mars" + userName);
+		System.out.println("********************************************************************************");
+		System.out.println("1.Edit exited Mission");
+		System.out.println("2.Log Out");
+		System.out.println("Please enter your choice:");
 	}
 	public boolean displayLoginSuccessful() {
 		return true;
--- Validation.java
+++ Validation.java
@@ -6,7 +6,7 @@ public class Validation {
     public Validation(){
 
     }
-
+/*
     public int acceptNumericInput() {
         int temp = 0;
         boolean isValidate = false;
@@ -22,9 +22,11 @@ public class Validation {
         }while(isValidate == false);
 
         return temp;
-    }
+    }*/
 
+    /*
     public int acceptValidateLoginChoice(){
+
         int temp = 0;
         boolean isValidate = false;
 
@@ -33,6 +35,29 @@ public class Validation {
             if (jacky.hasNextInt()) {
                 temp = jacky.nextInt();
                 if(temp == 1 || temp == 2){
+                    isValidate = true;
+                }
+                else {
+                    System.out.println("Please enter the right choice");
+                }
+
+            } else {
+                System.out.println("Error Pleas enter a number, Please enter again:");
+            }
+        }while(isValidate == false);
+
+        return temp;
+    }*/
+
+    public int acceptValidateChoiceInRange(int min, int max){
+        int temp = 0;
+        boolean isValidate = false;
+
+        do {
+            Scanner jacky = new Scanner(System.in);
+            if (jacky.hasNextInt()) {
+                temp = jacky.nextInt();
+                if(min <=temp && temp <= max){
                     isValidate = true;
                 }
                 else {
