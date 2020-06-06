22a0d56da676bdbcf9b5c228853410a8ed126512
Gaohe Li <63134205+GaoheLi-42@users.noreply.github.com>
2020-06-07 01:46:48 +1000

    Update Criteria.java

--- Criteria.java
+++ Criteria.java
@@ -1,50 +1,62 @@
 public class Criteria {
-	private String criminalRecords;
-	private String healthRecords;
-	private String[] qualifications;
-	private int criteriaID; //add a new attribute and mutator and accessor ---Jiancong
-	/*
-	add a default constructor  ---Jiancong
-	 */
-	public Criteria(){
-		criminalRecords = "";
-		healthRecords = "";
-		qualifications = new String[20];
-		criteriaID = 0;
-	}
 	
-	public Criteria(String criminalRecords, String healthRecords, String[] qualifications) {
-		super();
-		this.criminalRecords = criminalRecords;
-		this.healthRecords = healthRecords;
-		this.qualifications = qualifications;
-	}
-	public String getCriminalRecords() {
-		return criminalRecords;
-	}
-	public void setCriminalRecords(String criminalRecords) {
-		this.criminalRecords = criminalRecords;
-	}
-	public String getHealthRecords() {
-		return healthRecords;
-	}
-	public void setHealthRecords(String healthRecords) {
-		this.healthRecords = healthRecords;
-	}
-	public String[] getQualifications() {
-		return qualifications;
-	}
-	public void setQualifications(String[] qualifications) {
-		this.qualifications = qualifications;
-	}
+	//private int criteriaID; //add a new attribute and mutator and accessor ---Jiancong
+	/*
+// 	add a default constructor  ---Jiancong
+// 	 */
+// 	public Criteria(){
+// 		criminalRecords = "";
+// 		healthRecords = "";
+// 		qualifications = new String[20];
+// 		criteriaID = 0;
+// 	}
+	
+	private int missionId;
+    private String criminalRecords;
+    private String healthRecords;
+    private ArrayList<String[]> qualifications;
 
-	public int getCriteriaID() {
-		return criteriaID;
-	}
+    public Criteria(){
 
-	public void setCriteriaID(int criteriaID) {
-		this.criteriaID = criteriaID;
-	}
-}
+    }
+
+    public Criteria(int missionId, String criminalRecords, String healthRecords, ArrayList<String[]> qualifications) {
+        this.missionId = missionId;
+        this.criminalRecords = criminalRecords;
+        this.healthRecords = healthRecords;
+        this.qualifications = qualifications;
+    }
+
+    public int getMissionId() {
+        return missionId;
+    }
+
+    public void setMissionId(int missionId) {
+        this.missionId = missionId;
+    }
+
+    public String getCriminalRecords() {
+        return criminalRecords;
+    }
+
+    public void setCriminalRecords(String criminalRecords) {
+        this.criminalRecords = criminalRecords;
+    }
+
+    public String getHealthRecords() {
+        return healthRecords;
+    }
+
+    public void setHealthRecords(String healthRecords) {
+        this.healthRecords = healthRecords;
+    }
+
+    public ArrayList<String[]> getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(ArrayList<String[]> qualifications) {
+        this.qualifications = qualifications;
+    }
 
 }
