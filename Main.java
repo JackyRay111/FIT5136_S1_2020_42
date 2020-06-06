00403bd61fa821a8e8c71919deee39310e3500d4
Gaohe Li <63134205+GaoheLi-42@users.noreply.github.com>
2020-06-06 21:18:11 +1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,21 @@ package FIT5136_Implementation;
 
 public class Main {
 
+  private ArrayList<String> readFile()
+    {
+        ArrayList<String> option = new ArrayList<String>();
+        try
+        {
+            FileIO readFile = new FileIO("criteria.txt");
+            option = readFile.readFile();
+        }
+        catch (Exception e) 
+        {
+            System.out.println("It can not find the criteria.txt");
+            System.exit(0);
+        }
+        return option;
+    }
 
 }
 
